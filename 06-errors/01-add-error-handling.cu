913b89c61f659e04c19cdb7f22f9ade5417eea67
sangminpark9 <52732279+sangminpark9@users.noreply.github.com>
2024-08-18 20:15:09 +0900

    error handling

---
+++ 06-errors/01-add-error-handling.cu
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+void init(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    a[i] = i;
+  }
+}
+
+__global__
+void doubleElements(int *a, int N)
+{
+
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  int stride = gridDim.x * blockDim.x;
+
+  for (int i = idx; i < N; i += stride)
+  {
+    a[i] *= 2;
+  }
+}
+
+bool checkElementsAreDoubled(int *a, int N)
+{
+  int i;
+  for (i = 0; i < N; ++i)
+  {
+    if (a[i] != i*2) return false;
+  }
+  return true;
+}
+
+int main()
+{
+  int N = 10000;
+  int *a;
+
+  size_t size = N * sizeof(int);
+  cudaMallocManaged(&a, size);
+
+  init(a, N);
+
+  size_t threads_per_block = 1024;
+  /* size_t threads_per_block = 2048; */
+  size_t number_of_blocks = 32;
+
+  cudaError_t syncErr, asyncErr;
+
+  doubleElements<<<number_of_blocks, threads_per_block>>>(a, N);
+
+############################ 
+  syncErr = cudaGetLastError();
+  asyncErr = cudaDeviceSynchronize();
+
+
+  if (syncErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(syncErr));
+  if (asyncErr != cudaSuccess) printf("Error: %s\n", cudaGetErrorString(asyncErr));
+
+############################ 
+
+  bool areDoubled = checkElementsAreDoubled(a, N);
+  printf("All elements were doubled? %s\n", areDoubled ? "TRUE" : "FALSE");
+
+  cudaFree(a);
+}
